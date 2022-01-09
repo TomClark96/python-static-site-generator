2f12062f17833b4faf67da9d82deb00a9c596a9a
thomasclark <tomclark96@aol.com>
2022-01-09 12:21:57 +0000

    Find root directory

---
+++ ssg/site.py
@@ -0,0 +1,11 @@
+import Path from pathlib
+
+class site():
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest
+
+
